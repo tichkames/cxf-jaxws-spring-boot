19fa5f3f0e1848b5b7320aefe9d88f710e4d2eb5
Tich <tkames@hotmail.com>
2020-03-25 12:48:12 +0200

    Added SpringRunner for junit 4

--- src/test/java/com/codenotfound/SpringCxfApplicationTests.java
+++ src/test/java/com/codenotfound/SpringCxfApplicationTests.java
@@ -4,10 +4,13 @@ import com.codenotfound.client.HelloWorldClient;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 public class SpringCxfApplicationTests {
 
